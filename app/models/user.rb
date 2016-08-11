10fed079e08032380dfd516c76fc757cdb5de767
ErnestasKlibavicius <ernestas.klibavicius@gmail.com>
2016-08-11 15:06:30 -0400

    Fixing user join table relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
+   #Things this user created
    has_many :messages, class_name: 'Message', foreign_key: 'creator_id'  
-   has_many :read_messages, class_name: 'Message', through:'message_reader'
    has_many :created_teams, class_name: 'Team', foreign_key: 'creator_id'
-   has_many :teams, class_name: 'Team', through: 'team_member'    
    has_many :created_channels, class_name: 'Channel', foreign_key: 'creator_id'
+
+   # Join Table relationship
+   has_many :team_members
+   has_many :channel_members
+   has_many :message_readers
+
+   # Things this user belongs to 
+   has_many :read_messages, class_name: 'Message', through:'message_readers'
+   has_many :teams, class_name: 'Team', through: 'team_member'    
    has_many :channels, class_name: 'Channel', through: 'channel_members'
 end
