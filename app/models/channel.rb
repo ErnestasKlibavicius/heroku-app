17a8a6dff6b5c712a9b644f59ec75603a2643ccb
ErnestasKlibavicius <ernestas.klibavicius@gmail.com>
2016-08-11 14:54:49 -0400

    in channel file fixed syntax error

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,7 +1,7 @@
 class Channel < ApplicationRecord
-  belongs_to :creator, class_name 'User', foreign_key: 'creator_id'
+  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   belongs_to :team
   has_many :messages
-  has_many :members, class_name 'User', through: 'channel_members'
+  has_many :members, class_name: 'User', through: 'channel_members'
  
 end
